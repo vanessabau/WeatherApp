0f4a11d617be5897b6e22fd66c5dcf868671d2d3
Vanessa Bautista <vbautista916@icloud.com>
2020-05-09 07:14:09 -0700

    created search history button on click

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,19 @@
 $(document).ready(function () {
+
+    var cityLinks = document.querySelectorAll("a");
+    
     //DISPLAY CURRENT DAY IN JUMBOTRON
     $("#date").html(moment().format("MMMM DD, YYYY").toString());
-
-    //VARIABLES FOR SEARCH ITEMS
-    var citySearch;
-    
-    //localStorage.getItem(JSON.parse(citySearch))
-    if (JSON.parse(localStorage.getItem('history'))) {
-         citySearch = JSON.parse(localStorage.getItem('history'))
-    } else {
-         citySearch = []
-    }
-    console.log(citySearch);
-    /*citySearch.each(
-        var liOfSearch = $("<li>");
-            liOfSearch.attr("class", `list-group-item`);
-            liOfSearch.val(cityLookUp.toUpperCase())
-            liOfSearch.text(cityLookUp.toUpperCase());
-            $("#searchList").append(liOfSearch);
-    )*/
-    $.each(citySearch, function(index, value) {
-        console.log(value);
-        //create those li in here
-    })
-
+ 
+    //GET 1 DAY WEATHER DATA
     function get1DayWeather(e) {
         e.preventDefault();
-        //save this city to history
+        //save city to local history
         var cityLookUp = $("#city-lookup").val().trim();
-    
-        // Here we are building the URL we need to query the database
+        var city = cityLookUp.toUpperCase();
+        localStorage.setItem('city', city);
+
+        // Weather Call URL
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityLookUp + "&appid=166a433c57516f51dfab1f7edaed8413";
         
         // AJAX call to OpenWeatherMap API
@@ -38,88 +22,120 @@ $(document).ready(function () {
             method: 'GET'
         }).then(function (response) {
            
-            //render the weather forecast
-            var iconCode1 = response.weather[0].icon;
-            var temp = response.main.temp;
-            var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-            var humidity = response.main.humidity;
-            var windSpeed = response.wind.speed;
-            var lon = response.coord.lon;
-            var lat = response.coord.lat;
-            var timezone = response.timezone;
+        //Variables to render weather forecast
+        var iconCode1 = response.weather[0].icon;
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        var humidity = response.main.humidity;
+        var windSpeed = response.wind.speed;
+        var lon = response.coord.lon;
+        var lat = response.coord.lat;
+        var searchHistory = $("<div class='card searchHistory'>").html("<button type='button' class='btn btn-link'>"+city+"</button>");
 
-            //Transfer to HTML
-            $("#city2Day").text(cityLookUp.toUpperCase());
-            citySearch.push(cityLookUp.toUpperCase())
-            localStorage.setItem('history', JSON.stringify(citySearch))
-            
-            $("#icon2Day").attr("src", "http://openweathermap.org/img/wn/" + iconCode1 + "@2x.png");
-            $("#temp2Day").text("Temperature: " + tempF.toFixed(2) + "°F");
-            $("#humidity2Day").text("Humidity: " + humidity);
-            $("#wind2Day").text("Wind Speed: " + windSpeed);
-            //console.log(cityLookUp.toUpperCase());
-            /*var liOfSearch = $("<li>");
-            liOfSearch.attr("class", `list-group-item`);
-            liOfSearch.val(cityLookUp.toUpperCase())
-            liOfSearch.text(cityLookUp.toUpperCase());
-            $("#searchList").append(liOfSearch);*/
-            
-            function testTest(params) {
-                params.preventDefault()
+        //Transfer to HTML
+        $("#city2Day").text(city);
+        $("#icon2Day").attr("src", "http://openweathermap.org/img/wn/" + iconCode1 + "@2x.png");
+        $("#temp2Day").text("Temperature: " + tempF.toFixed(2) + "°F");
+        $("#humidity2Day").text("Humidity: " + humidity);
+        $("#wind2Day").text("Wind Speed: " + windSpeed);
+        $("#searchList").append(searchHistory);
 
-                //function recursion - get the weather forecast
-            }
-            $(".list-group-item").click(testTest);
-        
-            //UV Index API call
-            function getUv(lat, lon) {
-                var queryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=166a433c57516f51dfab1f7edaed8413&lat="+lat+"&lon=" + lon;
-                $.ajax({
-                    url: queryURL,
-                    method: 'GET'
-                }).then(function (response) {
-                    $("#uv2Day").text("UV Index: "+ response.value);
-                })
+        //UV Index API call
+        function getUv(lat, lon) {
+            //UV Index API Call
+            var queryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=166a433c57516f51dfab1f7edaed8413&lat="+lat+"&lon=" + lon;
+            $.ajax({
+                url: queryURL,
+                method: 'GET'
+            }).then(function (response) {
+                $("#uv2Day").text("UV Index: "+ response.value);
+            })
             }
             getUv(lat, lon);
         })
-
         get5day(cityLookUp);
     }
 
-    
     //5 Day forcast API call
-    function get5day(city) {
-        var city = $("#city-lookup").val().trim();
-    
-        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=166a433c57516f51dfab1f7edaed8413";
+    function get5day(city5) {
+        var city5 = $("#city-lookup").val().trim();
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city5 + "&appid=166a433c57516f51dfab1f7edaed8413";
         
+        //5 Day forcast Ajax
         $.ajax({
             url: queryURL,
             method: 'GET'
         }).then(function (response) {
-            
+            $("#forcast").empty();  
 
             for (var i = 0; i < 5; i++) {
                 var tempF5 = ((response.list[i].main.temp - 273.15) * 1.80 + 32).toFixed(2);
                 var humidity5 = response.list[i].main.humidity;
                 var iconCode5 = response.list[i].weather[0].icon;
-
-                var forcastDiv = $("<div class='card col' id='eachDay'>").html(
-                    "Day: " + (i+1) + "<br>" +
+                var forcastDiv = $("<div class='card col' id='eachDay'>").html("Day: " + (i+1) + "<br>" +
                     "Temperature: "+ tempF5 + "°F <br>"+
                     "Humidity: " + humidity5 + "<br>" +
                     "<img src='http://openweathermap.org/img/wn/" + iconCode5 + "@2x.png' style='width: 30px'></div>"
                 );
-
                 $("#forcast").append(forcastDiv);
             }
         })
     }
-    
-
+  
     //on click launch function
     $("#submitBtn").on("click", get1DayWeather);
 
-    //on click listener to get the city from localstorage
+    //on click for search history buttons
+    $("#searchList").on("click", ".btn-link", function(){
+        console.log("worked");
+    });
 })
+
+
+
+ //on click launch function
+    //$(".clickMe").on("click", testing);
+
+    //on click listener to get the city from localstorage
+   // $("a").on("click", function(e){
+       // e.preventDefault();
+       // alert("worked");
+    //});*/
+ //console.log(cityLookUp.toUpperCase());
+            /*var liOfSearch = $("<li>");
+            liOfSearch.attr("class", `list-group-item`);
+            liOfSearch.val(cityLookUp.toUpperCase())
+            liOfSearch.text(cityLookUp.toUpperCase());
+            $("#searchList").append(liOfSearch);*/
+
+
+ /* 
+ 
+ function testTest(params) {
+        params.preventDefault()
+        alert("works");
+        //function recursion - get the weather forecast
+    }
+    $(".clickMe").click(testTest);
+ 
+ var citySearch;
+    citySearch.push(cityLookUp.toUpperCase())
+            localStorage.setItem('history', JSON.stringify(citySearch))
+            
+    //localStorage.getItem(JSON.parse(citySearch))
+    if (JSON.parse(localStorage.getItem('history'))) {
+         citySearch = JSON.parse(localStorage.getItem('history'))
+    } else {
+         citySearch = []
+    }
+    console.log(citySearch);
+    citySearch.each(
+        var liOfSearch = $("<li>");
+            liOfSearch.attr("class", `list-group-item`);
+            liOfSearch.val(cityLookUp.toUpperCase())
+            liOfSearch.text(cityLookUp.toUpperCase());
+            $("#searchList").append(liOfSearch);
+    )
+    $.each(citySearch, function(index, value) {
+        console.log(value);
+        //create those li in here
+    })*/
